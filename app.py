f449d28215413b1993c7c03ee681a1cae283d188
isaiah majarreis <80748304+isaiahmajarreis@users.noreply.github.com>
2023-04-19 18:15:08 +0800

    Implemented Digit Recognition and Prediction
    The functionalities were added alongside image preprocessing and utilizing the already existing model to make predictions.

--- app.py
+++ app.py
@@ -1,30 +1,47 @@
-from flask import Flask, render_template
+import os
+import cv2
+import numpy as np
+import tensorflow as tf
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
-import os
-import tensorflow as tf
-import cv2
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dev'
 app.config['UPLOAD_FOLDER'] = 'static/files'
 
+# Load the model
+model = tf.keras.models.load_model('digit_recognition.model')
+
+
 class UploadFileForm(FlaskForm):
     file = FileField('File')
     submit = SubmitField('Upload File')
 
-model = tf.keras.models.load_model('digit_recognition.model')
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-    form1 = UploadFileForm()
-    if form1.validate_on_submit():
-        file = form1.file.data # First grab the file
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
+    form = UploadFileForm()
+    data = None
+    if form.validate_on_submit():
+        file = form.file.data
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
 
-    return render_template('index.html', form=form1)
+        # Load the image and preprocess it
+        img = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (28, 28))
+        img = np.invert(np.array([img]))
+        img = img.astype('float32') / 255.0
+
+        # Get the prediction from the model
+        prediction = model.predict(img)
+        data = np.argmax(prediction)
+
+    return render_template('index.html', form=form, data=data)
 
 
 if __name__ == '__main__':
